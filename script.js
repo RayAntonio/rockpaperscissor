4dcc2c44907cf47d39079d9cd6cab15f66cdbec4
RayAntonio <136002290+RayAntonio@users.noreply.github.com>
2025-06-01 21:46:12 -0400

    Added a function that gives a random number between 1-3.

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Hello World")
+
+function getComputerChoice() {
+    computer_choice =Math.trunc(Math.random()*3+1)
+    //Expected output 1,2, or 3
+    return computer_choice;
+    
+}
